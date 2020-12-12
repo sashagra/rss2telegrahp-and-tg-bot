cd8e653d6c4f65993cf97eaa1d0fa379a0d085f3
granthin <archon.san@gmail.com>
2020-12-12 11:30:53 +0300

    small: add logs for news parsing

--- parsers/parseRss.js
+++ parsers/parseRss.js
@@ -5,7 +5,7 @@ const { getNewsLinks } = require('./rssMatching')
 
 module.exports = async () => {
   const rssNewData = []
-
+  console.log('RSS parsing...')
   let feed = await parser.parseURL(RSS_URL);
   feed.items.forEach(item => {
     rssNewData.push({title: item.title, link: item.link + '/full', date: item.pubDate});
